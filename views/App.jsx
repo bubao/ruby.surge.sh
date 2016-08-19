5468a1e7fd2cb35886a501ec5f4588c68d4902d7
pennyjs <pennyjs@shinchou.io>
2016-08-19 23:45:09 +0800

    pennyjs <pennyjs@shinchou.io>

--- views/App.jsx
+++ views/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-module.exports = React.createClass({
+module.exports = connect((state) => {
+  return {
+    users: []
+  }
+})(React.createClass({
+  componentDidMount() {
+    console.log(this.props)
+    console.log(this.state)
+  },
   render() {
     return <div>App</div>
   }
-})
+}))
