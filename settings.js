928cedf8aefc5f928ffcfd1d554570e20e5370ed
pennyjs <pennyjs@shinchou.io>
2016-09-06 21:48:28 +0800

    pennyjs <pennyjs@shinchou.io>

---
+++ settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  i18n: {
+    zh_CN: {
+      users: '用户',
+      topics: '主题',
+      login: '登录',
+      logout: '退出',
+      latest: '最新',
+      search: '搜索',
+      settings: '设置',
+      password: '密码',
+      username: '用户名',
+      pages_404: '抱歉，未找到该页面'
+    }
+  }
+}
