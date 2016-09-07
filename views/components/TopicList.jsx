eba0da2fc572ab2b7d5a876b7accdea65fdad7fd
pennyjs <pennyjs@shinchou.io>
2016-09-07 23:16:28 +0800

    pennyjs <pennyjs@shinchou.io>

--- views/components/TopicList.jsx
+++ views/components/TopicList.jsx
@@ -5,12 +5,14 @@ module.exports = React.createClass({
   renderRow() {
     return this.props.dataSource.map((item) => {
       return <li key={item.id}>
-        <Link to={`/topics/${item.id}`}>{item.title}</Link>
+        <Link to={`/topics/${item.id}`} className="item-title">{item.title}</Link>
+        <Link to={`/topics/${item.id}`} className="item-avatar">
+        </Link>
       </li>
     })
   },
   render() {
-    return <div>
+    return <div className="topic-items">
       <ul>{this.renderRow()}</ul>
     </div>
   }
